43d05445054f69e562602ddceb2ab9abd218692f
Mckenshel Cameau <mcameau20@gmail.com>
2026-03-20 13:23:07 -0400

    refactored the model and made sure normalization only happened by sector

---
+++ fetch_metrics.py
@@ -0,0 +1,110 @@
+import yfinance as yf 
+import pandas as pd
+from typing import List
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+def get_metrics(ticker:yf.Ticker):
+    info = ticker.info
+    # value
+    price_to_earnings = info.get('forwardPE')
+    price_to_book = info.get('priceToBook')
+    if not price_to_earnings:
+        earnings_yield = 0
+    else:
+        earnings_yield = 1 / price_to_earnings
+    
+    # momentum and volatility
+    momentum_1m, momentum_3m, volatility_daily, volatility_annual = get_momentum(ticker)
+
+    #quality
+    return_on_equity = info.get('returnOnEquity')
+    debt_to_equity = info.get('debtToEquity')
+
+    #size
+    market_cap = info.get('marketCap')
+
+   
+
+    return [price_to_earnings,price_to_book,earnings_yield,return_on_equity,debt_to_equity,
+    market_cap,momentum_1m,momentum_3m,volatility_daily,volatility_annual]
+
+def get_momentum(ticker:yf.Ticker) -> List[float]:
+    
+    hist = ticker.history(period="6mo")
+    if len(hist) < 63:
+        raise ValueError("Not enough historical data for 6M momentum")
+
+    price_today = hist['Close'].iloc[-1]
+    price_1m_ago = hist['Close'].iloc[-21] # ~about 21 trading days in 1 month
+    price_3m_ago = hist['Close'].iloc[-63]
+    
+    return_1m = (price_today - price_1m_ago)/price_1m_ago
+
+    return_3m = (price_today - price_3m_ago)/price_3m_ago
+
+    volatility_daily = hist['Close'].pct_change().std()
+
+    
+    volatility_annual = volatility_daily * (252 ** 0.5)
+
+    return [return_1m, return_3m, volatility_daily,volatility_annual]
+
+def process_ticker(ticker: yf.Ticker):
+    try:
+        info = ticker.info
+        metrics = get_metrics(ticker)
+
+        ticker_data = {
+            'Symbol': info.get('symbol'),
+            'Name': info.get('longName'),
+            'Sector': info.get('sector'),
+            'Price_to_Earnings': metrics[0],
+            'Price_to_Book': metrics[1],
+            'Earnings_Yield': metrics[2],
+            'Return_on_Equity': metrics[3],
+            'Debt_to_Equity': metrics[4],
+            'Market_Cap': metrics[5],
+            'Momentum_1m': metrics[6],
+            'Momentum_3m': metrics[7],
+            'Volatility_Daily': metrics[8],
+            'Volatility_Annual': metrics[9],
+        }
+        print(f"Downloaded metrics for {info.get('symbol')}")
+        return ticker_data
+    except Exception as e:
+        # Optional: log and skip failures
+        print(f"Failed for {ticker.ticker}: {e}")
+        return None
+
+def get_tickers(tickers_file_path):
+    # converts the tickers in the Symbol column of the data frame into a list
+
+    screened_stocks_df = pd.read_csv(tickers_file_path)
+    all_tickers = screened_stocks_df['Symbol'].to_list()
+    all_tickers = [yf.Ticker(ticker) for ticker in all_tickers]
+
+    return all_tickers
+
+def create_metrics_df(tickers_file_path:str) -> pd.DataFrame :
+    # creates of data frame with the list of tickers 
+
+    all_tickers = get_tickers(tickers_file_path)
+    
+    all_metrics = []
+
+    print("Downloading metrics for all tickers")
+
+    with ThreadPoolExecutor(max_workers=4 ) as executor:
+        futures = [executor.submit(process_ticker, t) for t in all_tickers]
+
+        for future in as_completed(futures):
+            result = future.result()
+            if result is not None:
+                all_metrics.append(result)
+
+    df = pd.DataFrame(all_metrics)
+    df = df.sort_values(by=['Sector'])
+
+    return df
+    
+
--- model.py
+++ model.py
@@ -1,44 +1,72 @@
 import pandas as pd
 import numpy as np
 
-
-def normalize(df: pd.DataFrame) -> pd.DataFrame:
+# use z-score normalization, setting a cap at 2.5 std 
+def normalize(df):
     mean = df.mean()
     std = df.std()
-    return (df - mean) / std
+
+    z_score = (df - mean) / std
+
+    return z_score.clip(-2.5,2.5)
+
+def calculate_factors(df):
+# calculate overall factor metrics, will come up with better ones later
 
 
-if __name__ == "__main__":
-    df = pd.read_csv("metrics.csv")
-    # drop rows with missing values for now
-    df = df.dropna()
-    # Momentum, Volatility, Quality, Value, Market Cap
-    weights = np.array([0.2, 0.2, 0.2, 0.2, 0.2])
+    df["Overall_Momentum"] = (df["Momentum_1m"] + df["Momentum_3m"])/2
 
-    metrics_columns = ['Price to Earnings', 'Price to Book', 'Earnings Yield', 'Return on Equity', 'Debt to Equity', 'Market Cap','Momentum 3m', 
-    'Momentum 6m', 'Volatility Daily', 'Volatility Annual']
-
-
-    # normalize all metrics
-    for column in metrics_columns:df[column] = normalize(df[column])
-
-
-    # calculate overall factor metrics, will come up with better ones later
-    df["Overall Momentum"] = normalize((df["Momentum 3m"] + df["Momentum 6m"])/2)
-    df["Overall Volatility"] = normalize(-(df["Volatility Daily"] + df["Volatility Annual"])/2)
+    df["Overall_Volatility"] = -(df["Volatility_Daily"] + df["Volatility_Annual"])/2
 
     # return on equity is positive, debt to equity is negative to avoid companies with high debt
-    df["Overall Quality"] = normalize((df["Return on Equity"] - df["Debt to Equity"])/2)
-    df["Overall Value"] = normalize((df["Earnings Yield"] + df["Price to Book"])/2)
+    df["Overall_Quality"] = (df["Return_on_Equity"] - df["Debt_to_Equity"])/2
 
-    values = np.array(df[['Overall Momentum', 'Overall Volatility', 'Overall Quality', 'Overall Value','Market Cap']])
+    df["Overall_Value"] = (df["Earnings_Yield"] + df["Price_to_Book"])/2
 
-    # take inner product of weights and normalized df to get score
-    df['Score'] = values @ weights
+    return df
 
 
-    df.sort_values(by=['Sector', 'Score'], ascending=False,inplace=True)
-    final_df= df[['Symbol', 'Name', 'Sector', 'Score','Overall Momentum', 'Overall Volatility', 'Overall Quality', 'Overall Value','Market Cap']]
-    final_df.to_csv("scores.csv", index=False)
+def calculate_scores(df,weights:np.array):
+    """
+    weight must be a 5x1 column vector where:
+    [momentum, volatility, quality, value, market cap]
+    Multiplies factors by weights to create score for each stock
+
+    For instance if the weights are all .2, for one stock a score 
+    would be a linear combination of :
+    Score = beta_1 * .2 + beta_2 * .2 + beta_3 * .2 + beta_4 * .2 + beta_5 * .2
+    """
+
+    # creates a M X 5 matrix where M is the number of stocks and 5 is the number of factors
+    values = np.array(df[['Overall_Momentum', 'Overall_Volatility', 'Overall_Quality', 'Overall_Value','Market_Cap']])
+
+    df["Score"] = values @ weights
+
+    return df
+
+def build_scores(metrics_file_path,weights:np.array):
+
+    df =  pd.read_csv(metrics_file_path)
+
+    df = df.dropna()
+
+    metrics_columns = ['Price_to_Earnings', 'Price_to_Book', 'Earnings_Yield', 'Return_on_Equity', 'Debt_to_Equity', 'Market_Cap','Momentum_1m', 
+    'Momentum_3m', 'Volatility_Daily', 'Volatility_Annual']
+
+    for column in metrics_columns:
+        # only normalizing by sector
+        df[column] = df.groupby('Sector')[column].transform(normalize)
+
+    df = calculate_factors(df)
+
+    df = calculate_scores(df,weights)
+
+    df = df.sort_values(by=['Sector', 'Score'], ascending=False)
+    
+    final_df= df[['Symbol', 'Name', 'Sector', 'Score','Overall_Momentum', 'Overall_Volatility', 'Overall_Quality', 'Overall_Value','Market_Cap']]
+    
+    return final_df
+
+
 
 
